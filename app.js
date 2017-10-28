c6c7af128381b34b121dc0fb094658c7e96735d2
santy_naren <santhoshnarendra@gmail.com>
2017-10-28 17:35:21 +0530

    loader added

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
   //Fire store part 
+
+
+  function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
+
   var config = {
     apiKey: "AIzaSyAMBINMcRp7uLSbDA-hT3P2A6MWLbKRYso",
     authDomain: "wedding-cbec7.firebaseapp.com",
